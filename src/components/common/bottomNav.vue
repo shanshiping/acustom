55bea960b51b0a1afb0c7823562b3ed242b1ba31
plus <767832499@qq.com>
2018-07-25 09:05:15 +0800

    更新bottomNav

--- src/components/common/bottomNav.vue
+++ src/components/common/bottomNav.vue
@@ -4,7 +4,7 @@
       <div class="nav-item-name">首页</div>
     </router-link>
     <router-link class="nav-item sort" to="/sort">
-      <div class="nav-item-name sort">分类</div>
+      <div class="nav-item-name">分类</div>
     </router-link>
     <router-link class="nav-item cart" to="/cart">
       <div class="nav-item-name">购物车</div>
@@ -34,63 +34,57 @@ export default {
   position: fixed;
   bottom: 0;
   width: 100%;
-}
-.navImg {
-  width: 58px;
-  height: 53px;
-  vertical-align: -3px;
-}
-.nav-item {
-  flex: 1;
- font-family: pingFangSC-Light, sans-serif;
- font-size: 24px;
-  text-decoration: none;
-  padding-top: 0.2rem;
-}
-.nav-item:link,
-.nav-item:visited {
+  font-size: 0;
   background-color: white;
-  color: black;
-}
-.nav-item:hover,
-.nav-item:active {
-  color: black;
-}
-.nav-item {
-  background-position:  center 6px;
-  background-repeat: no-repeat;
-}
-.home.nav-item {
-  background-image: url("../../assets/img/bottomNav/home.png");
-  
-}
-.sort.nav-item {
-  background-image: url("../../assets/img/bottomNav/sort.png");
-}
-.cart.nav-item {
-  background-image: url("../../assets/img/bottomNav/buy.png");
-}
-.mine.nav-item {
-  background-image: url("../../assets/img/bottomNav/mine.png");
-}
-.home.nav-item:hover,
-.home.nav-item:active {
-  background-image: url("../../assets/img/bottomNav/home_on.png");
-}
-.sort.nav-item:hover,
-.sort.nav-item:active {
-  background-image: url("../../assets/img/bottomNav/sort_on.png");
-}
-.cart.nav-item:hover,
-.cart.nav-item:active {
-  background-image: url("../../assets/img/bottomNav/buy_on.png");
-}
-.mine.nav-item:hover,
-.mine.nav-item:active {
-  background-image: url("../../assets/img/bottomNav/mine_on.png");
-}
-.nav-item-name {
-  margin-top: 66px;
+  .nav-item {
+    flex: 1;
+    font-family: pingFangSC-Light, sans-serif;
+    font-size: 24px;
+    text-decoration: none;
+    padding-top: 4px;
+    background-position: center 6px;
+    background-repeat: no-repeat;
+    .nav-item-name {
+      margin-top: 66px;
+    }
+  }
+  .nav-item:link,
+  .nav-item:visited {
+    background-color: white;
+    color: black;
+  }
+  .nav-item:hover,
+  .nav-item:active {
+    color: black;
+  }
+  .home.nav-item {
+    background-image: url("../../assets/img/bottomNav/home.png");
+  }
+  .sort.nav-item {
+    background-image: url("../../assets/img/bottomNav/sort.png");
+  }
+  .cart.nav-item {
+    background-image: url("../../assets/img/bottomNav/buy.png");
+  }
+  .mine.nav-item {
+    background-image: url("../../assets/img/bottomNav/mine.png");
+  }
+  .home.nav-item:hover,
+  .home.nav-item:active {
+    background-image: url("../../assets/img/bottomNav/home_on.png");
+  }
+  .sort.nav-item:hover,
+  .sort.nav-item:active {
+    background-image: url("../../assets/img/bottomNav/sort_on.png");
+  }
+  .cart.nav-item:hover,
+  .cart.nav-item:active {
+    background-image: url("../../assets/img/bottomNav/buy_on.png");
+  }
+  .mine.nav-item:hover,
+  .mine.nav-item:active {
+    background-image: url("../../assets/img/bottomNav/mine_on.png");
+  }
 }
 </style>
 
